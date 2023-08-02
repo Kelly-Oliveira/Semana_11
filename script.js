fbbb7c0e7f911c46e7f4cc6df237b1e11c0b7831
Kelly Oliveira <79283215+Kelly-Oliveira@users.noreply.github.com>
2023-08-02 00:21:51 -0300

    Projeto finalizado

---
+++ script.js
@@ -0,0 +1,42 @@
+const list = document.getElementById('list')
+const card = document.getElementById('card')
+
+async function getList() {
+  try {
+    const response = await fetch('https://digimon-api.vercel.app/api/digimon')
+    const digimons = await response.json()
+    renderList(digimons)
+  }
+  catch(erro) {
+    console.error("Capturei um erro: ", erro)
+  }
+}
+
+// let renderList = (items) => items.forEach((item) => list.innerHTML += 
+// `<input class="buttons" type="submit" value=${item.name} >`);
+
+function renderList(items) {
+  items.forEach((item) => list.innerHTML +=
+  `<input class="buttons" type="submit" value=${item.name} onclick="getDigimon(this.value)">`)
+}
+
+async function getDigimon(nome) {
+  try {
+    const response = await fetch(`https://digimon-api.vercel.app/api/digimon/name/${nome}`)
+    const digimon = await response.json()
+    renderDigimon(digimon[0])
+  }
+  catch(erro) {
+    console.error("Capturei um erro: ", erro)
+  }
+}
+
+function renderDigimon(digimon) {
+  card.innerHTML = `
+    <img src=${digimon.img}>
+    <h3>Name: ${digimon.name}</h3>
+    <p>Level: ${digimon.level}</p>
+  `
+}
+
+getList()
